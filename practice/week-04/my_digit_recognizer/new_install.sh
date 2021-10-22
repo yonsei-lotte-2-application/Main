396e8085d113972eeb81eb838f7868d965c05782
jw-temp <wsbtgt@gmail.com>
2021-10-22 13:31:59 +0900

    update week-04

---
+++ practice/week-04/my_digit_recognizer/new_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt install python3-venv libgl1-mesa-glx -y
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
+pip install Flask
+pip install Pillow
+pip install opencv-python
+pip install joblib
+pip install tensorflow --no-cache-dir
+pip install sklearn
